5f28a881827cf7cb0edb9ea15e502c204457638a
Souvik <souvik.bagchi@gmail.com>
2026-02-07 06:54:26 -0600

    FE: Move index.html to docs folder

--- src/ts/autintellandingpage/svelte.config.js
+++ src/ts/autintellandingpage/svelte.config.js
@@ -1,6 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		prerender: {
+			entries: ['*']
+		},
+		paths: {
+			base: '/landingpage'
+		}
+	}
+};
 
 export default config;
